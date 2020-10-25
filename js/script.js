10432a94cff59699741f3f67f1771c468759a6c3
Phellipe Sander <phellipe.sander1@gmail.com>
2020-10-25 20:09:19 -0300

    animacoes e slide

---
+++ js/script.js
@@ -0,0 +1,17 @@
+if (window.SimpleSlide){
+    
+    new SimpleSlide({
+        slide: "quote",
+        time: 5000
+    })
+    
+    new SimpleSlide({
+        slide: "portifolio",
+        nav: true,
+        time: 5000
+    })      
+
+}
+if (window.SimpleAnime){
+    new SimpleAnime();
+}
